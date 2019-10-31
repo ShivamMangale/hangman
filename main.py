1efcaf1966c2af45c9d3f1acb3f97673a5d769aa
ShivamMangale <l3pp1n@gmail.com>
2019-10-31 06:11:03 +0400

    shell completed

---
+++ main.py
@@ -0,0 +1,53 @@
+#change code to implement abstraction ,i.e., oops
+import numpy as np
+import getch
+root = "groot"
+#add code to make sure valid root
+wordlength = len(root)
+
+root = list(root)
+
+chartrack = np.empty(26)
+chartrack.fill(0)
+
+answer = ""
+initial = "-"
+for i in range(wordlength):
+	answer = answer + initial
+
+#print(answer)
+
+for i in range(wordlength):
+	print(answer[i],end = "")
+print()
+
+answer = list(answer)
+
+attempts = 0
+
+limit = 10#make the attempt limit more realistic, dynamic wrt the word taken as root
+
+remaining = wordlength
+
+while attempts < limit :
+	print("Enter your guess : ",end = "",flush = True)#handle why not printing before getch
+	attempts = attempts + 1
+	guess = getch.getche()
+	#add code to make sure no penalization for duplicate attempts
+	#add code to make sure no invalid guesses
+	print()
+	if guess in root:
+		for i in range(wordlength):
+			if(root[i] == guess):	
+				answer[i] =	root[i]
+				remaining = remaining - 1
+	if remaining <= 0:
+		print("Congrats!!! You got it right!!! =b=b")
+		break
+	print("Current state : ",end = "")
+	for i in range(wordlength):
+		print(answer[i],end = "")
+	print(" and remaining attempts are ", limit - attempts)
+if remaining > 0 :
+	print("Mission Failed. We'll get them next time.")
+print("Reached end of game")
